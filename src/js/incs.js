c923789b7db586484c2539bc1c57acb7aab3a6fc
Michael James <micrjames921@gmail.com>
2022-12-20 13:37:22 -0700

    implemented modal to display the recipe listings

---
+++ src/js/incs.js
@@ -0,0 +1,7 @@
+const body = document.body;
+const main = body.children.namedItem("main");
+
+const products = main.children.namedItem("products");
+const results = main.children.namedItem("results");
+
+export { products, results };
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
-// add JavaScript code here
+import { products, results } from "./incs.js";
+
+const resultsHdr = results.children.namedItem("modal-header");
+const resultsCloseBtn = resultsHdr.children.namedItem("modal-header-close");
+const resultsHdrText = resultsHdr.children[1];
+resultsHdrText.textContent = "Recipes";
+
+for(const product of products.children) {
+    for(const child of product.children) {
+	    if(child.tagName == "BUTTON")
+		   child.addEventListener("click", () => {
+			   results.classList.remove("hidden");
+			   window.scrollTo(0, 0);
+		   });
+	}
+}
+
+resultsCloseBtn.addEventListener("click", () => {
+    results.classList.add("hidden");
+});
