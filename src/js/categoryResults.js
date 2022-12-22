3ff78a292257ba95da8b29a247da76458cc4a57a
Michael James <micrjames921@gmail.com>
2022-12-22 03:59:51 -0700

    got working 'category results cycling' feature

--- src/js/categoryResults.js
+++ src/js/categoryResults.js
@@ -1,6 +1,31 @@
 import { createSpan, createBtn, createBtnGroup, removeChildren } from "./DOMutils.js";
 import { titleCase } from "./utils.js";
 
+const mealsPerPage = 6;
+let whereSliceStart;
+let sliceEnd;
+const setCategoryResults = function(categoryResults, meals, sliceStart) {
+   whereSliceStart = sliceStart;
+   sliceEnd = sliceStart + mealsPerPage;
+   let mealsSlice;
+   if(meals.length <= mealsPerPage) mealsSlice = meals;
+   else {
+	  mealsSlice = meals.slice(sliceStart, sliceEnd);
+   }
+
+   createCategoryResults(categoryResults, meals, mealsSlice);
+};
+const createCategoryResults = function(categoryResults, meals, mealsSlice) {
+   for(const meal of mealsSlice) {
+       const categoryResult = createCategoryResult(meal);   
+       categoryResults.appendChild(categoryResult);
+   }
+
+   const categoryResultsBtnGroup = createCategoryBtnGroup(categoryResults, meals);
+   categoryResults.appendChild(categoryResultsBtnGroup);
+
+   return categoryResultsBtnGroup.children;
+};
 const createCategoryResult = function(meal) {
    const categoryResult = document.createElement("div");                                    
    categoryResult.setAttribute("class", "category-result")
@@ -13,9 +38,30 @@ const createCategoryResult = function(meal) {
 
    return categoryResult;
 };
-const createCategoryBtnGroup = function() {
+const createCategoryBtnGroup = function(categoryResults, meals) {
    const categoryResultsPrevBtn = createBtn("category-results-prev-btn", "btn", "less-than");
    const categoryResultsNextBtn = createBtn("category-results-next-btn", "btn", "greater-than");
+
+   if(whereSliceStart == 0) {
+	   categoryResultsPrevBtn.disabled = true;
+   } 
+   if(meals.length - sliceEnd > mealsPerPage){
+	   categoryResultsNextBtn.disabled = true;
+   }
+   categoryResultsPrevBtn.addEventListener("click", function() {
+	  whereSliceStart-=6;
+	  sliceEnd = whereSliceStart+6;
+	  const mealsSlice = meals.slice(whereSliceStart, sliceEnd);
+	  resetCategoryResults(categoryResults);
+	  createCategoryResults(categoryResults, meals, mealsSlice);
+   });
+   categoryResultsNextBtn.addEventListener("click", function() {
+	  whereSliceStart+=6;
+	  sliceEnd = whereSliceStart+6;
+	  const mealsSlice = meals.slice(whereSliceStart, sliceEnd);
+	  resetCategoryResults(categoryResults);
+	  createCategoryResults(categoryResults, meals, mealsSlice);
+   });
    const categoryResultsBtnGroup = createBtnGroup("category-results-btn-group", [categoryResultsPrevBtn, categoryResultsNextBtn]);
 
    return categoryResultsBtnGroup;
@@ -36,12 +82,8 @@ const createResultTitle = function(title) {
    return categoryResultTitle;
 };
 
-const setCategoryResult = function(meal) {
-   const categoryResult = createCategoryResult(meal);
-   return categoryResult;
-};
 const resetCategoryResults = function(categoryResults) {
    removeChildren(categoryResults);
 };
 
-export { setCategoryResult, resetCategoryResults, createCategoryBtnGroup };
+export { setCategoryResults, resetCategoryResults };
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { products, results, resultsCloseBtn, resultsHdrText, recipeResults, categoryResults, categoriesData } from "./incs.js";
 import { titleCase, ignoreText } from "./utils.js";
-import { setCategoryResult, resetCategoryResults, createCategoryBtnGroup } from "./categoryResults.js";
+import { setCategoryResults, resetCategoryResults } from "./categoryResults.js";
 
 for(const product of products.children) {
     for(const child of product.children) {
@@ -10,12 +10,7 @@ for(const product of products.children) {
 			   resultsHdrText.textContent = `${titleCase(slicedId)} Recipes`;
 			   results.classList.remove("hidden");
 
-			   for(const meal of categoriesData[slicedId].meals) {
-				  const categoryResult = setCategoryResult(meal);	
-				  categoryResults.appendChild(categoryResult);
-			   }
-			   const categoryResultsBtnGroup = createCategoryBtnGroup();
-			   categoryResults.appendChild(categoryResultsBtnGroup);
+			   setCategoryResults(categoryResults, categoriesData[slicedId].meals, 0);
 
 			   categoryResults.classList.remove("hidden");
 
